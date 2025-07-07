d6bfe9bfd0e977a31c39a21b89d0603c7328b004
farah <farahhaymoni05@gmail.com>
2025-07-07 16:09:13 +0000

    edit ex00

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -8,13 +8,19 @@ int main(int argc, char *argv[])
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return 0;
     }
-    for (int i = 1; i < argc; ++i)
+    int i = 1;
+    while (argv[i])
     {
-        std::string arg = argv[i];
-        for (size_t j = 0; j < arg.length(); ++j)
-            std::cout << (char)std::toupper(arg[j]);
+	    int j = 0;
+	    while (argv[i][j]){ 
+		    std::cout << (char)std::toupper(argv[i][j]);
+	    j++;
+	    }
+	    if (i < argc - 1)
+		    std::cout << " ";
+	    i++;
     }
     std::cout << std::endl;
-    return 0;
+	return(0);
 }
 
