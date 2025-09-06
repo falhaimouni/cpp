d4eb33d6d32b35f073775464e41b0c5de6bdbf67
Farah Alhaimouni <falhaimo@c2r6s3.42amman.com>
2025-09-06 16:41:27 +0300

    m06-ex00

---
+++ Module06/ex00/ScalarConverter.hpp
@@ -0,0 +1,33 @@
+#ifndef SCALAR_CONVERTER_HPP
+#define SCALAR_CONVERTER_HPP
+
+#include <iostream>
+#include <string>
+#include <limits>
+#include <cmath>
+#include <iomanip>
+#include <cctype>
+
+class ScalarConverter {
+private:
+    ScalarConverter(); // Private constructor
+    ScalarConverter(const ScalarConverter&);
+    ScalarConverter& operator=(const ScalarConverter&);
+    ~ScalarConverter();
+
+    static void printChar(double d);
+    static void printInt(double d);
+    static void printFloat(double d);
+    static void printDouble(double d);
+
+    static bool isCharLiteral(const std::string& str);
+    static bool isInt(const std::string& str);
+    static bool isFloat(const std::string& str);
+    static bool isDouble(const std::string& str);
+    static bool isPseudoLiteral(const std::string& str);
+
+public:
+    static void convert(const std::string& str);
+};
+
+#endif
